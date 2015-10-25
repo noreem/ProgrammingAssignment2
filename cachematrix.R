85e0abb207dbe455519915f26f097fab6a8bf438
Adrian Arhire <arhireadrian@gmail.com>
2015-10-25 12:13:23 +0200

    Solve Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,45 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix function is exactly like the makeVector function
+## in the example but instead of mean we store the inverse
+## getinverse & setinverse functions sets and gets the inverse
+
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inv) inverse <<- inv
+  getinverse <- function() inverse
+  
+  list(set = set, get = get,
+      setinverse = setinverse,
+      getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve, as in the example, computes the inverse(in the example the mean) 
+## only if it wasn't previously computed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  inverse <- x$getinverse()
+  
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  
+  inverse    
 }
